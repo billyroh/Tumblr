47c59809efb6effc223b474200202d993fdfedd5
Billy Roh <billy.roh@gmail.com>
2014-07-05 11:09:11 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Billy Roh on 7/5/14.
+//  Copyright (c) 2014 Billy Roh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
